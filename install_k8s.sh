df0badf9fe42d4da65ff960fc7f1c2a7d88fe15a
dksx <37441849+dksx@users.noreply.github.com>
2021-03-22 02:45:26 +0100

    Create install_k8s.sh

---
+++ install_k8s.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+export OS=xUbuntu_18.04
+export VERSION=1.20
+export POD_SUBNET=10.0.0.0/24
+export BIND_ADDRESS=$(hostname -i)
+export PUBLIC_IP=
+
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+swapoff -a
+
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
+EOF
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+EOF
+
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers-cri-o.gpg add -
+
+sudo apt-get update
+sudo apt-get install -y cri-o cri-o-runc
+sudo systemctl daemon-reload
+sudo systemctl enable crio --now
+
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl daemon-reload
+
+curl -s https://gist.githubusercontent.com/dksx/526e5e7b4101af1a73bfb13ddb3fef18/raw/2b9303c7e27e1e0b3a7f77f2004be840402eb7aa/config.yaml > config.yaml
+sed -i "s/POD_SUBNET/$POD_SUBNET/" config.yaml
+sed -i "s/BIND_ADDRESS/$BIND_ADDRESS/" config.yaml
+sed -i "s/PUBLIC_IP/$PUBLIC_IP/" config.yaml
+
+
+sudo kubeadm init --config=config.yaml
